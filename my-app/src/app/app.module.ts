921b51cdb18e5c463c09dd0f07ec8c81e1ee4a8a
David Ihl <daih1985@gmail.com>
2023-07-24 09:13:53 +0200

    Update repository

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyFirstComponentComponent } from './my-first-component/my-first-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyFirstComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
